db8ec1d14a0760460c480eed30dd4d14f0753133
criogenesis <ztuttle99@gmail.com>
2022-07-05 16:35:59 -0400

    Added some line functionality to MessageManager.cpp, added some more type definitions and functions into the MessageManager.hpp interface and Connection.hpp interface.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include <algorithm>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, world! Test" << std::endl;
+    std::string str = "H e l l o";
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    std::cout << str; // Output Hello
+    
     return 0;
 }
